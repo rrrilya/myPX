c15930bb36f92aabc41b0c5b594a6c72a522855a
rrrilya <130274524+rrrilya@users.noreply.github.com>
2024-12-02 22:21:23 +0500

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -31,5 +31,6 @@ class Settings(BaseSettings):
     COMPLETE_QUESTS: bool = True
     COMPLETE_DANGER_TASKS: bool = False
     WATCH_ADS: bool = True
+    USE_ALL_CHARGES: bool = True
 
 settings = Settings()  # type: ignore
